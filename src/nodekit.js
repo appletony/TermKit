d0681b56625d54cf12ce925f841de8693b39130a
Sérgio Ramos <mail@sergioramos.me>
2012-11-24 03:11:04 +0000

    insert license in the readme

--- src/nodekit.js
+++ src/nodekit.js
@@ -1,12 +1,4 @@
-#!/usr/bin/env node
-var termkit = {
-  version: 1,
-};
-
-require.paths.unshift(__dirname + '/../Shared/');
-
-// Load requirements.
-var http = require('http'),  
+var http = require('http'),
     io = require('socket.io-node'),
     router = require("./router");
 
@@ -14,16 +6,16 @@ var http = require('http'),
 var config = require('./config').getConfig();
 
 // Set up http server.
-var server = http.createServer(function (request, result) { 
-//  result.writeHeader(200, {'Content-Type': 'text/html'}); 
+var server = http.createServer(function (request, result) {
+//  result.writeHeader(200, {'Content-Type': 'text/html'});
 //  result.writeBody('<h1>TermKit</h1>');
-//  result.finish(); 
+//  result.finish();
 });
 
 server.listen(2222);
 
 // Set up WebSocket and handlers.
-var ioServer = io.listen(server); 
+var ioServer = io.listen(server);
 ioServer.sockets.on('connection', function (client) {
   var p = new router.router(client);
 });
