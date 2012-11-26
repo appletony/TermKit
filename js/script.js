9ca44b596e01ac550c21692603c1a2e41015809d
Sérgio Ramos <mail@sergioramos.me>
2012-11-26 00:43:53 +0000

    update script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 var el = document.getElementById('anchor');
-var location = window.location;
 
-if(!location.host.match(/.github.com$/)) return el.href = location.protocol + '//github.com/unconed/TermKit';
+if(!window.location.host.match(/.github.com$/)) return el.href = window.location.protocol + '//github.com/unconed/TermKit';
 
-var user = location.host.match(/(.*?)\.github\.com/i)[1]
+var user = window.location.host.match(/(.*?)\.github\.com/i)[1]
 
-el.href = location.protocol + '//github.com/' + user + '/TermKit';
+el.href = window.location.protocol + '//github.com/' + user + '/TermKit';
